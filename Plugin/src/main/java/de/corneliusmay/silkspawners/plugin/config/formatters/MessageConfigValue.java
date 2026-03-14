813e8aac7fe18d580af3529cf1398ad2a687b8a0
Julius <141836004+JuliusH1@users.noreply.github.com>
2026-03-14 13:54:48 +0100

    Enhance MessageConfigValue to support MiniMessage

--- Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/config/formatters/MessageConfigValue.java
+++ Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/config/formatters/MessageConfigValue.java
@@ -1,10 +1,27 @@
 package de.corneliusmay.silkspawners.plugin.config.formatters;
 
 import de.corneliusmay.silkspawners.plugin.config.handler.ConfigValueFormatter;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 
 public class MessageConfigValue implements ConfigValueFormatter<String> {
+
+    private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
+    private static final LegacyComponentSerializer LEGACY_SERIALIZER =
+            LegacyComponentSerializer.legacySection();
+
     @Override
     public String format(String value) {
-        return value.replaceAll("(?<!\\\\)\\$" , "§").replace("\\$", "$");
+        if (isMiniMessage(value)) {
+            return LEGACY_SERIALIZER.serialize(MINI_MESSAGE.deserialize(value));
+        }
+        return value.replaceAll("(?<!\\\\)\\$", "§").replace("\\$", "$");
+    }
+
+    private boolean isMiniMessage(String value) {
+        int open = value.indexOf('<');
+        if (open < 0) return false;
+        int close = value.indexOf('>', open + 1);
+        return close > open;
     }
 }
